48f9e77720a501233d5e0bd7497b43a5c97abb84
Aaron Perez <hawkboy1111@yahoo.com>
2018-02-23 19:35:03 -0800

    test
    first commit

---
+++ Test/src/main.java
@@ -0,0 +1,7 @@
+
+public class main {
+	public static void main(String[] args) {
+		secondary secondaryObject = new secondary();
+		secondaryObject.shallom();
+	}
+}
---
+++ Test/src/secondary.java
@@ -0,0 +1,6 @@
+
+public class secondary {
+	public void shallom() {
+		System.out.println("SHALLOM");
+	}
+}
